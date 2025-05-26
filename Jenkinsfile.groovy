c4a90e9325ea902e222efcfa418a20471e4e9fa4
MeherJOffice <meher@darasa.io>
2025-05-26 13:38:46 +0400

    Fix building on production

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -263,8 +263,7 @@ pipeline {
                     when {
                         expression {
                             return params.GAME_ENGINE == 'unity' &&
-                           (params.COCOS_VERSION == 'cocos2' || params.COCOS_VERSION == 'cocos3') &&
-                           params.ENVIRONMENT == 'Testing'
+                           (params.COCOS_VERSION == 'cocos2' || params.COCOS_VERSION == 'cocos3')
                         }
                     }
                     steps {
